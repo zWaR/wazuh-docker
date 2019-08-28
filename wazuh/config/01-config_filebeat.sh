8825e57f408aec964f94b545fb3f61b84aebaf90
Jose M <jm.garcia@wazuh.com>
2019-08-28 10:45:59 +0200

    Removing unneeded certs from filebeat config script

--- wazuh/config/01-config_filebeat.sh
+++ wazuh/config/01-config_filebeat.sh
@@ -6,34 +6,24 @@ set -e
 WAZUH_FILEBEAT_MODULE=wazuh-filebeat-0.1.tar.gz
 
 if [ "$ELASTICSEARCH_URL" != "" ]; then
-  >&2 echo "Customize Elasticsearch ouput IP."
+  >&2 echo "Customize Elasticsearch ouput IP"
   sed -i 's|http://elasticsearch:9200|'$ELASTICSEARCH_URL'|g' /etc/filebeat/filebeat.yml
 fi
 
 # Configure filebeat.yml security settings
 
 if [ "$SSL_CERTIFICATE_AUTHORITIES" != "" ]; then
-  >&2 echo "Customize Elasticsearch ouput IP."
-  sed -i 's|#ssl.certificate_authorities:|'ssl.certificate_authorities:\ [\"$SSL_CERTIFICATE\"]'|g' /etc/filebeat/filebeat.yml
-fi
-
-if [ "$SSL_CERTIFICATE" != "" ]; then
-  >&2 echo "Customize Elasticsearch ouput IP."
-  sed -i 's|#ssl.certificate:|'ssl.certificate:\ \"$SSL_CERTIFICATE\"'|g' /etc/filebeat/filebeat.yml
-fi
-
-if [ "$SSL_KEY" != "" ]; then
-  >&2 echo "Customize Elasticsearch ouput IP."
-  sed -i 's|#ssl.key:|'ssl.key:\ \"$SSL_KEY\"'|g' /etc/filebeat/filebeat.yml
+  >&2 echo "Configuring Certificate Authorities."
+  sed -i 's|#ssl.certificate_authorities:|'ssl.certificate_authorities:\ [\"$SSL_CERTIFICATE_AUTHORITIES\"]'|g' /etc/filebeat/filebeat.yml
 fi
 
 if [ "$USERNAME" != "" ]; then
-  >&2 echo "Customize Elasticsearch ouput IP."
+  >&2 echo "Configuring username."
   sed -i 's|#username:|'username:\ \"$USERNAME\"'|g' /etc/filebeat/filebeat.yml
 fi
 
 if [ "$PASSWORD" != "" ]; then
-  >&2 echo "Customize Elasticsearch ouput IP."
+  >&2 echo "Configuring password."
   sed -i 's|#password:|'password:\ \"$PASSWORD\"'|g' /etc/filebeat/filebeat.yml
 fi
 
