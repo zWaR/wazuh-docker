3680df41799e733df2279f30619f1b70141196b0
Jose M <jm.garcia@wazuh.com>
2019-08-28 10:47:50 +0200

    Update entrypoint.sh

--- wazuh/config/entrypoint.sh
+++ wazuh/config/entrypoint.sh
@@ -8,6 +8,5 @@ done
 
 # Configuring and starting Wazuh-Manager, Wazuh-API and Filebeat
 chkconfig --add wazuh-manager && service wazuh-manager start
-sed -i 's|YOUR_ELASTIC_SERVER_IP|'elasticsearch'|g' /etc/filebeat/filebeat.yml
 chkconfig --add filebeat && service filebeat start
 /usr/bin/node /var/ossec/api/app.js > /dev/null 2>&1
